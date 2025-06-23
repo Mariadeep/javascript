cc18f8c65b79c4db728ffbbd3c3053bd8d99ffdd
Mariadeep <mariamaddeerg@gmail.com>
2025-06-23 11:45:32 +0530

    adding this file

---
+++ function.js
@@ -0,0 +1,46 @@
+//syntax
+// function name (){}
+
+function org (){
+var a=20;
+console.log(a);
+
+let b=50;
+console.log(b);
+
+const c=60;
+console.log(c);
+
+ } 
+org();
+
+//function types
+//function statemnet and declaration---> paramaters and arguments
+function antif (hi){
+    console.log(hi)
+}
+antif("javascript");
+
+//function expression or anonymous function
+let a=function hello (hi){
+    console.log (hi)
+}
+a ("javascrizycoonpt");
+
+let b= function (ammo){
+    console.log(ammo);
+}
+b("hello captain");
+
+imediatte invoke function expressions
+(function (iife){
+    console.log(iife);
+    
+})("function");
+
+// arrow function
+// syntax
+// ()=>{}
+
+let d(arrow)=>(console.log(arrow))
+d("arrow function")
